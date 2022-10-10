04978b8386ea2501cc7cbb00f0bec3df8457aa5d
Petlyra <bazkus34>
2022-10-10 12:36:10 +0300

    initial commit

---
+++ src/test/java/yurii/palei/springcloudconfig/SpringCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package yurii.palei.springcloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
