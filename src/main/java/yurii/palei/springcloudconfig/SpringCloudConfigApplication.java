101064d481e38650b487388b42b0c94fefef10d5
Petlyra <bazkus34>
2022-10-10 17:15:42 +0300

    fixes

--- src/main/java/yurii/palei/springcloudconfig/SpringCloudConfigApplication.java
+++ src/main/java/yurii/palei/springcloudconfig/SpringCloudConfigApplication.java
@@ -3,11 +3,9 @@ package yurii.palei.springcloudconfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableWebSecurity
 public class SpringCloudConfigApplication {
 
   public static void main(String[] args) {
