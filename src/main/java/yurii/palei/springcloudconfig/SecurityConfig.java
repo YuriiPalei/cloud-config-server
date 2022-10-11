d09e7109fdb2f67e49343f48867292e81f8f753b
Petlyra <bazkus34>
2022-10-11 11:21:21 +0300

    basic auth

--- src/main/java/yurii/palei/springcloudconfig/SecurityConfig.java
+++ src/main/java/yurii/palei/springcloudconfig/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
-  protected void configure(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity.csrf().disable();
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
   }
 }
